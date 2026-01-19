04c25c191242783de0b67fa2e7f647d3accc5725
Thomas Lebeau <lebeau.thomas@gmail.com>
2026-01-19 18:31:02 +0100

    fix: switch to HashRouter to resolve GitHub Pages 404 on reload

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import RecipeList from "./RecipeList";
 import Recipe from "./Recipe";
 
 export default function App() {
   return (
-    <BrowserRouter basename="/recettes/">
+    <HashRouter>
       <Routes>
         <Route path="/" element={<RecipeList />} />
         <Route path="/:slug" element={<Recipe />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
